16e65ec34d4cc37bf0b63c6e6fe398aacc709d10
Usama <sammirza201@gmail.com>
2024-10-20 22:36:03 +0500

    fixed user list id

--- Frontend/src/components/UserAvatar/UserListItem.tsx
+++ Frontend/src/components/UserAvatar/UserListItem.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Avatar, Box, Typography, Paper } from '@mui/material';
 import { UserInfo } from '../../Context/ChatProvider';
-// import { ChatState } from '../../Context/ChatProvider';
 
 interface UserListItemProps {
     handleFunction: () => void; 
     user: UserInfo;
 }
 
-
 const UserListItem: React.FC<UserListItemProps> = ({user, handleFunction }) => {
+    console.log("User ID:", user._id);
     
     return (
+        
         <Paper
             elevation={3}
             onClick={handleFunction}
             sx={{
+            
                 cursor: 'pointer',
                 backgroundColor: '#E8E8E8',
                 '&:hover': {
                     backgroundColor: '#38B2AC',
                     color: 'white',
                 },
-                width: '100%',
+                width: '90%',
                 display: 'flex',
                 alignItems: 'center',
                 color: 'black',
@@ -31,14 +32,14 @@ const UserListItem: React.FC<UserListItemProps> = ({user, handleFunction }) => {
                 borderRadius: '8px',
             }}
         >
-            {/* User Avatar */}
+           
             <Avatar
                 sx={{ marginRight: 2, cursor: 'pointer' }}
                 alt={user.name}
                 src={user.pic}
             />
 
-            {/* User Info */}
+    
             <Box>
                 <Typography variant="subtitle1" component="div">
                     {user.name}
@@ -48,7 +49,9 @@ const UserListItem: React.FC<UserListItemProps> = ({user, handleFunction }) => {
                 </Typography>
             </Box>
         </Paper>
+        
     );
 };
+
 
 export default UserListItem;
