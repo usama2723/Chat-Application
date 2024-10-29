2ee01484fd614cff1318d828bfc0ae57e661b423
Usama <sammirza201@gmail.com>
2024-10-29 18:04:21 +0500

    added group chat functionality

--- Frontend/src/components/GroupChats.tsx
+++ Frontend/src/components/GroupChats.tsx
@@ -1,79 +1,223 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, ReactNode, useState } from "react";
 import {
   Dialog,
   Typography,
   Button,
-  TextField,
   Box,
-  IconButton
+  IconButton,
+  FormControl,
+  InputLabel,
+  OutlinedInput
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import { ChatState, UserInfo } from "../Context/ChatProvider";
+import axios from "axios";
+import { toast } from "react-toastify";
+import UserListItem from "./UserAvatar/UserListItem";
+import UserBadgeItem from "./UserAvatar/UserBadgeItem";
 
-const GroupChats = ({ openModal, setOpenModal }: {
+
+const GroupChats = ({
+  openModal,
+  setOpenModal,
+  children,
+}: {
   openModal: boolean;
+  children?: ReactNode;
   setOpenModal: Dispatch<SetStateAction<boolean>>;
 }) => {
-  return (
-    <Dialog
-      open={openModal}
-      onClose={() => setOpenModal(false)}
-      maxWidth="md"
-      PaperProps={{
-        sx: {
-          height: "50vh",
-          width: "30%",
-          backgroundColor: "#2a3942",
-          color: "white",
-          padding: 2,
-          position: 'relative'
+  const [groupChatName, setGroupChatName] = useState<string | undefined>();
+  const [selectedUsers, setSelectedUsers] = useState<UserInfo[]>([]);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const { user, chats, setChats } = ChatState();
+
+  const handleGroup = (userToAdd: UserInfo) => {
+    if (selectedUsers.includes(userToAdd)) {
+      toast.warning('User already added');
+      return;
+    }
+
+    setSelectedUsers([...selectedUsers, userToAdd]);
+  };
+
+  const handleSearch = async (query: string) => {
+    setSearch(query);
+    if (!query) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user?.token}`,
         },
-      }}
-    >
-      {/* Close Button */}
-      <IconButton
-        sx={{
-          position: 'absolute',
-          top: 8,
-          right: 8,
-          color: 'white',
+      };
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_API_BASE_URL}/user?search=${search}`,
+        config
+      );
+      console.log(data);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast.error('Error Searching');
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (!groupChatName || !selectedUsers) {
+      toast.warning('Please fill all the fields');
+      return;
+    }
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user?.token}`,
+        },
+      };
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_BASE_URL}/chat/group`,
+        {
+          name: groupChatName,
+          users: JSON.stringify(selectedUsers.map((u) => u._id)),
+        },
+        config
+      );
+      setChats([data, ...chats]);
+      setOpenModal(false);
+      toast.success('New Group Chat Created!');
+    } catch (error) {
+      toast.error('Failed to Create the Chat!');
+    }
+  };
+
+  const handleDelete = (delUser: UserInfo) => {
+    setSelectedUsers(selectedUsers.filter((sel) => sel._id !== delUser._id));
+  };
+  return (
+    <>
+      <Dialog
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        maxWidth="md"
+        PaperProps={{
+          sx: {
+            height: "50vh",
+            width: "30%",
+            backgroundColor: "#2a3942",
+            color: "white",
+            padding: 2,
+            position: 'relative'
+          },
         }}
-        onClick={() => setOpenModal(false)}
       >
-        <CloseIcon />
-      </IconButton>
-
-      <Typography
-        variant="h6"
-        component="div"
-        sx={{ margin: 3, textAlign: "center" }}
-      >
-        Group Chat
-      </Typography>
-
-      <Box mb={2}>
-        <TextField
+        <IconButton
           sx={{
-            color: "white",
-            "& fieldset": { borderColor: "white" },
-            "& label": { color: "white" },
+            position: 'absolute',
+            top: 8,
+            right: 8,
+            color: 'white',
           }}
-          fullWidth
-          variant="outlined"
-          label="Search user"
-        />
-      </Box>
-      <Box>
-        <TextField
-          sx={{
-            color: "white",
-            "& fieldset": { borderColor: "white" },
-            "& label": { color: "white" },
-          }}
-          fullWidth
-          variant="outlined"
-          label="Enter name"
-        />
+          onClick={() => setOpenModal(false)}
+        >
+          <CloseIcon />
+        </IconButton>
+
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ margin: 3, textAlign: "center" }}
+        >
+          Group Chat
+        </Typography>
+
+        <Box mb={2}>
+          <FormControl fullWidth variant="outlined">
+            <InputLabel
+              sx={{
+                color: "white",
+                "&.Mui-focused": {
+                  color: "white",
+                },
+              }}
+              htmlFor="component-outlined"
+            >
+              Chat Name
+            </InputLabel>
+            <OutlinedInput
+              id="component-outlined"
+              sx={{
+                color: "white",
+                "& fieldset": { borderColor: "white" },
+                "& .MuiOutlinedInput-input": { color: "white" },
+                "&:hover fieldset": { borderColor: "white" },
+                "&.Mui-focused fieldset": { borderColor: "white" },
+              }}
+              fullWidth
+              label="Chat Name"
+              onChange={(e) => setGroupChatName(e.target.value)}
+
+            />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl fullWidth variant="outlined">
+            <InputLabel
+              sx={{
+                color: "white",
+                "&.Mui-focused": {
+                  color: "white",
+                },
+              }}
+              htmlFor="component-outlined"
+            >
+              Add Users
+            </InputLabel>
+            <OutlinedInput
+              id="component-outlined"
+              sx={{
+                color: "white",
+                "& fieldset": { borderColor: "white" },
+                "& .MuiOutlinedInput-input": { color: "white" },
+                "&:hover fieldset": { borderColor: "white" },
+                "&.Mui-focused fieldset": { borderColor: "white" },
+              }}
+              fullWidth
+              label="Add Users"
+              onChange={(e) => handleSearch(e.target.value)}
+            />
+          </FormControl>
+
+        </Box>
+        <Box>
+          {selectedUsers.map((u) => (
+            <UserBadgeItem
+              key={u._id}
+              user={u}
+              handleFunction={() => handleDelete(u)}
+            />
+          ))}
+        </Box>
+        <Box m={2}>
+        {loading ? (
+          <div>Loading....</div>
+        ) : (
+          searchResult?.slice(0, 4).map((user) => (
+            <UserListItem
+              key={user._id}
+              user={user}
+              handleFunction={() => handleGroup(user)}
+            />
+          ))
+        )}
+        </Box>
+       
         <Button
+          onClick={handleSubmit}
           variant="contained"
           fullWidth
           sx={{
@@ -86,9 +230,11 @@ const GroupChats = ({ openModal, setOpenModal }: {
         >
           Create Group Chat
         </Button>
-      </Box>
-    </Dialog>
-  )
-}
+
+        {children}
+      </Dialog>
+    </>
+  );
+};
 
 export default GroupChats;
