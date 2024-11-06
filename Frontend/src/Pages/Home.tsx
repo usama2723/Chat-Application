0cee0076198bec0e8a51e680266f467b1cc8447c
Usama <sammirza201@gmail.com>
2024-11-06 23:21:26 +0500

    added argument

--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -1,11 +1,13 @@
 import { ChatThreadsContainer } from "../components/ChatThreadsContainer";
 import ConversationContainer from "../components/ConversationContainer";
 
-const Home = () => {
+const Home = ({ fetchAgain }: {
+  fetchAgain: boolean;
+}) => {
   return (
     <div className="flex h-screen py-5 px-5 bg-[#0c1317]">
       <div className="w-[30%] bg-[#111b21] text-white border-r overflow-y-scroll border-[#394b53] ">
-        <ChatThreadsContainer />
+        <ChatThreadsContainer fetchAgain = {fetchAgain} />
       </div>
 
       <div className="container flex-1 flex flex-col ">
