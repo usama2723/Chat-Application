bc309ef603aa90d264bb319916522c1bf832acdb
Usama <sammirza201@gmail.com>
2024-11-07 23:05:10 +0500

    fixed theme issue

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,11 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import ChatProvider from "./Context/ChatProvider.js";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
   <BrowserRouter>
     <ChatProvider>
       <App />
     </ChatProvider>
   </BrowserRouter>
+  </ThemeProvider>
 );
